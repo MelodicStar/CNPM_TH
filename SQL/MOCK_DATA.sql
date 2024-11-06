4f50e2fa3521d71c539a16be5da4b6cb64d6c20a
MelodicStar <dkhoa5596@gmail.com>
2024-11-07 01:23:48 +0700

    main-0

---
+++ SQL/MOCK_DATA.sql
@@ -0,0 +1,33 @@
+create table MOCK_DATA (
+	IdHS VARCHAR(50),
+	TenHS VARCHAR(50),
+	GioiTinh VARCHAR(50),
+	NamSinh DATE,
+	IdPH VARCHAR(50),
+	QuanHe VARCHAR(11)
+);
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('LZ070-WL', 'Falito Eagle', 'Male', '7/20/2024', '152-51', 'child');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('UP960-CJ', 'Nessa Lightewood', 'Genderfluid', '6/7/2024', '824-73', 'sibling');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('QQ752-WD', 'Raffarty Warin', 'Male', '8/19/2024', '666-47', 'niece');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('HY914-WO', 'Roldan Veel', 'Agender', '9/26/2024', '459-70', 'grandparent');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('NR874-QG', 'Humphrey Dagworthy', 'Male', '3/7/2024', '539-22', 'parent');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('GB069-RY', 'Corrianne Trousdell', 'Female', '8/14/2024', '775-76', 'parent');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('VS294-ZS', 'Maxie McCall', 'Male', '6/28/2024', '897-61', 'nephew');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('IR017-SF', 'Reece Jaskiewicz', 'Bigender', '3/11/2024', '426-67', 'child');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('NZ618-PC', 'Franciskus Jeness', 'Male', '10/1/2024', '458-47', 'cousin');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('MT357-AA', 'Kenon Binnion', 'Male', '11/16/2023', '165-40', 'sibling');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('EH985-JK', 'Katherine Rollins', 'Female', '7/6/2024', '758-74', 'uncle');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('PJ884-UR', 'Roxane Regan', 'Female', '12/22/2023', '105-39', 'child');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('IT928-RF', 'Erasmus Legrice', 'Male', '12/8/2023', '244-57', 'sibling');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('PM474-RW', 'Weider Buye', 'Male', '12/15/2023', '259-14', 'uncle');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('CK351-JL', 'Katie Eggleston', 'Female', '11/15/2023', '296-48', 'cousin');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('UK103-PX', 'Phedra Flieg', 'Female', '1/10/2024', '906-84', 'niece');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('AG790-XL', 'Shina Gullick', 'Female', '6/21/2024', '450-15', 'grandparent');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('HM849-ZE', 'Tara Bardey', 'Female', '8/3/2024', '348-11', 'grandparent');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('MB772-QS', 'Nessi Fall', 'Bigender', '6/9/2024', '588-75', 'sibling');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('WN534-YV', 'Georgianne Asey', 'Female', '10/28/2024', '729-81', 'nephew');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('JW962-TV', 'Natty Frederick', 'Male', '6/14/2024', '787-98', 'parent');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('VM455-WX', 'Kati Trainer', 'Female', '8/17/2024', '453-44', 'parent');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('FO917-QT', 'Pepito Corkill', 'Male', '11/23/2023', '792-68', 'aunt');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('LY289-BQ', 'Gaspar Geraudel', 'Male', '5/29/2024', '883-46', 'niece');
+insert into MOCK_DATA (IdHS, TenHS, GioiTinh, NamSinh, IdPH, QuanHe) values ('LF479-KA', 'Decca Emeney', 'Male', '2/8/2024', '891-10', 'uncle');
