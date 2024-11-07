8b3ed9ab3f35e5c8ace3bde0c205a77bc0242e46
MelodicStar <dkhoa5596@gmail.com>
2024-11-07 10:20:34 +0700

    main-1

---
+++ mamNonTuongLaiTuoiSang/Controllers/AccountController.cs
@@ -0,0 +1,95 @@
+﻿using mamNonTuongLaiTuoiSang.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace mamNonTuongLaiTuoiSang.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly ILogger<AccountController> logger;
+        private readonly HttpClient httpClient;
+
+        private readonly string urlPh = "http://localhost:5005/api/PhuHuynhs";
+        private readonly string urlNv = "http://localhost:5005/api/NhanViens";
+
+        public AccountController(IHttpContextAccessor httpContextAccessor, ILogger<AccountController> logger, HttpClient httpClient)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+            this.logger = logger;
+            this.httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(string email, string password)
+        {
+            try
+            {
+                var session = httpContextAccessor.HttpContext.Session;
+
+                // Kiểm tra thông tin người dùng PhuHuynh qua API
+                var phuHuynhResponse = await httpClient.GetAsync($"{urlPh}/filter?email={email}&matKhau={password}");
+                if (phuHuynhResponse.IsSuccessStatusCode)
+                {
+                    var parent = await phuHuynhResponse.Content.ReadFromJsonAsync<PhuHuynh>();
+                    if (parent != null)
+                    {
+                        session.SetString("UserEmail", parent.Email);
+                        session.SetString("Password", parent.MatKhau);
+                        ViewData["PhuHuynh"] = parent.IdPh;
+                        return RedirectToAction("PhuHuynh", "PhuHuynh", parent);
+                    }
+                }
+
+                // Kiểm tra thông tin người dùng NhanVien qua API
+                var nhanVienResponse = await httpClient.GetAsync($"{urlNv}/filter?email={email}&matKhau={password}");
+                if (nhanVienResponse.IsSuccessStatusCode)
+                {
+                    var user = await nhanVienResponse.Content.ReadFromJsonAsync<NhanVien>();
+                    if (user != null)
+                    {
+                        session.SetString("UserEmail", user.Email);
+                        session.SetString("Password", user.MatKhau);
+
+                        if (user.TenCv == "Admin")
+                        {
+                            return RedirectToAction("Index", "NhanVien", new { area = "Admin" });
+                        }
+                        else if (user.TenCv == "GiaoVien")
+                        {
+                            ViewData["GiaoVien"] = user.MaSt;
+                            ViewBag.GiaoVien = user.MaSt;
+                            return RedirectToAction("Index", "Teacher", new { id = user.MaSt });
+                        }
+                    }
+                }
+
+                ViewBag.ErrorMessage = "Invalid email or password";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while logging in.");
+                ViewBag.ErrorMessage = "An error occurred while accessing the database.";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            var session = httpContextAccessor.HttpContext.Session;
+            session.Clear();
+
+            return Ok();
+        }
+    }
+}
